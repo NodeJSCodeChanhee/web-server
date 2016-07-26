ea6a20d9f680cb8a91b6815edee3e5ab2e9cef04
InChan Hwang <neogeoss1@gmail.com>
2016-07-25 23:18:36 -0400

    Updated language

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(middleware.logger);
 }); */
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
-	res.send('About us!');
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
